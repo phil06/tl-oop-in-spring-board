658c4c818005813d19db2b5798306a07cdc75027
phil06 <oobtloo@naver.com>
2016-01-06 01:52:55 +0900

    게시판

---
+++ src/main/java/com/oop/board/dao/BoardDao.java
@@ -0,0 +1,22 @@
+package com.oop.board.dao;
+
+import java.util.List;
+
+import com.oop.board.domain.BoardVO;
+
+public interface BoardDao {
+
+	public abstract List<BoardVO> list();
+	
+	public abstract int delete(BoardVO boardVO);
+	
+	public abstract int deleteAll();
+	
+	public abstract int update(BoardVO boardVO);
+	
+	public abstract void insert(BoardVO boardVO);
+	
+	public abstract BoardVO select(int seq);
+	
+	public abstract int updateReadCount(int seq);
+}
